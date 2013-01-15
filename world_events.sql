634c2fd62a7778b1c7f6fe59cfd0026ca0e127ee
Cosmodor <test007@live.it>
2013-01-15 18:22:12 +0100

    Update world_events.sql

--- world_events.sql
+++ world_events.sql
@@ -6,6 +6,20 @@
  ToDo : Noblegarden, Hallow's End, Pilgrim's Bounty, Harvest Festival, Fireworks Spectacular
 */ 
 
+-- For 2013 (thx to Trinity Core)
+UPDATE `game_event` SET `start_time`='2013-01-27 00:01:00' WHERE `entry`=7; -- Lunar Festival
+UPDATE `game_event` SET `start_time`='2013-02-10 00:01:00' WHERE `entry`=8; -- Love is in the Air
+UPDATE `game_event` SET `start_time`='2013-03-31 00:01:00' WHERE `entry`=9; -- Noblegarden
+UPDATE `game_event` SET `start_time`='2013-04-28 00:01:00' WHERE `entry`=10; -- Children's Week
+UPDATE `game_event` SET `start_time`='2013-06-21 00:01:00' WHERE `entry`=1; -- Midsummer Fire Festival
+UPDATE `game_event` SET `start_time`='2013-09-13 00:01:00' WHERE `entry`=11; -- Harvest Festival
+UPDATE `game_event` SET `start_time`='2013-09-19 00:01:00' WHERE `entry`=50; -- Pirates' Day
+UPDATE `game_event` SET `start_time`='2013-09-20 00:01:00' WHERE `entry`=24; -- Brewfest
+UPDATE `game_event` SET `start_time`='2013-10-18 01:00:00' WHERE `entry`=11; -- Hallow's End
+UPDATE `game_event` SET `start_time`='2013-11-01 01:00:00' WHERE `entry`=51; -- Day of the Dead
+UPDATE `game_event` SET `start_time`='2013-11-24 01:00:00' WHERE `entry`=26; -- Pilgrim's Bounty
+UPDATE `game_event` SET `start_time`='2013-12-15 06:00:00' WHERE `entry`=2; -- Winter Veil
+
 
 -- Feast of Winter Veil 
 
