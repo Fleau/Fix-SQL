3d0333f8efae0703a9cf00803087ebb1bda6121e
Cosmodor <test007@live.it>
2013-03-10 21:22:02 +0100

    Update fix_worgen_area.sql

--- fix_worgen_area.sql
+++ fix_worgen_area.sql
@@ -20,7 +20,7 @@ UPDATE gameobject_template
 UPDATE quest_template 
   SET NextQuestIdChain = 14099,
       NextQuestId = 14099,
-	  ExclusiveGroup = -14093
+      ExclusiveGroup = -14093
   WHERE id in (14093,14098);
   
 UPDATE quest_template 
@@ -79,7 +79,7 @@ UPDATE quest_template SET PrevQuestId = 24903, NextQuestIdChain = 24678 WHERE id
 UPDATE quest_template SET PrevQuestId = 24920 , NextQuestIdChain = 24602 WHERE id = 24678;
 UPDATE quest_template SET PrevQuestId = 24678, NextQuestIdChain = 24679 WHERE id = 24602;
 UPDATE quest_template SET PrevQuestId = 24602 , NextQuestIdChain = 24680 WHERE id = 24679;
-UPDATE quest_template SET PrevQuestId = 24679, NextQuestIdChain = =24681 WHERE id = 24680;
+UPDATE quest_template SET PrevQuestId = 24679, NextQuestIdChain = 24681 WHERE id = 24680;
 UPDATE quest_template SET PrevQuestId = 24680 WHERE id = =24681;
 
 INSERT INTO smart_scripts ( entryorguid, source_type, id , link, event_type, event_phase_mask, event_chance, event_flags, event_param1, event_param2, event_param3, event_param4, action_type, action_param1, action_param2, action_param3, action_param4, action_param5, action_param6, target_type, target_param1, target_param2, target_param3, target_x, target_y, target_z, target_o, comment) VALUES
