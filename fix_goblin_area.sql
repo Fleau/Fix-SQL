bb5b8571d3357b488b263daa8535040ddbc431c4
Cosmodor <test007@live.it>
2013-03-10 21:19:31 +0100

    Some "," missing XP

--- fix_goblin_area.sql
+++ fix_goblin_area.sql
@@ -13,7 +13,7 @@
 */
 UPDATE quest_template 
   SET NextQuestId = 14115,
-      NextQuestIdChain = 14115
+      NextQuestIdChain = 14115,
       ExclusiveGroup= 14113
     WHERE Id IN (14113,14153);
 
@@ -38,7 +38,7 @@ UPDATE quest_template SET PrevQuestId = 14115,NextQuestIdChain = 14120
 */
 UPDATE quest_template 
   SET NextQuestId = 25473,
-      NextQuestIdChain = 25473
+      NextQuestIdChain = 25473,
       ExclusiveGroup= 14069
     WHERE Id IN (14069,14075);
 
@@ -50,8 +50,8 @@ UPDATE quest_template SET PrevQuestId = 24567, NextQuestIdChain = 24502 WHERE Id
 UPDATE quest_template SET PrevQuestId = 28349, NextQuestIdChain = 24567 WHERE Id = 24502;
 UPDATE quest_template 
   SET PrevQuestId = 24502,
-      NextQuestId = 24520 
-      NextQuestIdChain = 24520 
+      NextQuestId = 24520, 
+      NextQuestIdChain = 24520, 
       ExclusiveGroup = 28414
     WHERE Id  IN (28414,24503);--Fourth and Goal
 
